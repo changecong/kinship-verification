7b2bb2cf2ce6fc4fd33e403a2880ea6dffc5d23d
Zhicong Chen <changecong@gmail.com>
2013-10-23 01:57:45 -0400

    add image reader, unfinished yet

--- kinvrf/include/image_reader.h
+++ kinvrf/include/image_reader.h
@@ -4,7 +4,7 @@
  * Version:       
  * Author:        Zhicong Chen <zhicong.chen@changecong.com>
  * Created at:    Tue Oct 22 23:02:16 2013
- * Modified at:   Tue Oct 22 23:34:48 2013
+ * Modified at:   Wed Oct 23 01:47:58 2013
  * Modified by:   Zhicong Chen <zhicong.chen@changecong.com>
  * Status:        Experimental, do not distribute.
  * Description:   This is a class that read image in before processing. 
@@ -29,21 +29,43 @@ using namespace std;
 /// \brief
 class ImageReader {
 
+  public:
     /// \fn
     /// \brief
-    void ImageReader();
+    ImageReader();
 
     /// \fn
     /// \brief
-    explicit void ImageReader(string path);
+    explicit ImageReader(string path);
     
     /// \fn
-    /// \brief
-    explicit void ImageReader(ImageReader imageReader);
+    /// \brief a copy constructor
+    explicit ImageReader(const ImageReader &other);
 
+    /// \fn
+    /// \brief set and get image_path_
+    // void image_path(string path);
+    string image_path();
+
+    /// \fn
+    /// \brief set and get image
+    // void image(Mat image);
+    Mat image();
+
+    /// \fn
+    /// \brief set and get image
+    // void image_is_read(bool flag);
+    bool image_is_read();
+    
+
+  private:
     /// \fn init()
     /// \brief
     void init();
+
+    string image_path_;  ///< the path of image
+    Mat image_;  ///< the image that image reader reads in
+    bool image_is_read_;  ///< a flag that used to set if the image is read successfully
 };
 
 #endif KINVRF_IMAGE_READER_H
--- kinvrf/src/imagerelated/image_reader.cpp
+++ kinvrf/src/imagerelated/image_reader.cpp
@@ -4,11 +4,60 @@
  * Version:       
  * Author:        Zhicong Chen <zhicong.chen@changecong.com>
  * Created at:    Tue Oct 22 23:31:28 2013
- * Modified at:   Tue Oct 22 23:31:58 2013
+ * Modified at:   Wed Oct 23 01:55:44 2013
  * Modified by:   Zhicong Chen <zhicong.chen@changecong.com>
  * Status:        Experimental, do not distribute.
  * Description:   
  *
  */
 
-#include ""
+// corresponding header
+#include "../../inlcude/image_reader.h"
+
+// constructors
+ImageReader::ImageReader() {
+    // call the init()
+    init();
+}
+
+explicit ImageReader::ImageReader(string path) {
+    image_path_ = path;
+    
+    // get the image by opencv api
+    image_ = imread(image_path);
+
+    // check if the image is read successfully
+    if (image_.empty()) {
+        image_is_read_ = false;
+    } else {
+        image_is_read_ = true;
+    }
+}
+
+explicit ImageReader::ImageReader(const ImageReader &other) {
+    
+    // get other's image path
+    image_path_ = other.image_path();
+    // get other's image
+    image_ = other.image();
+    // get other's flag
+    image_is_read_ = other.image_is_read();
+
+}
+
+void IamgeReader::init() {
+    image_path_ = "";
+    image_is_read_ = false;
+}
+
+string ImageReader::image_path() {
+    return image_is_read_ ? image_path : "";
+}
+
+Mat ImageReader::image() {
+    return image_is_read_ ? image : image; 
+}
+
+bool ImageReader::image_is_read() {
+    return image_is_read_;
+}
