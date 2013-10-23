0903677169950420ef8a014a4f8d06502e7e9ff1
Zhicong Chen <changecong@gmail.com>
2013-10-23 15:35:45 -0400

    little update

--- kinvrf/test/opencv-test/mat-test.cpp
+++ kinvrf/test/opencv-test/mat-test.cpp
@@ -1,3 +1,16 @@
+/*
+ * Filename:      mat-test.cpp
+ * Copyright:     2013, Zhicong Chen
+ * Version:       
+ * Author:        Zhicong Chen <zhicong.chen@changecong.com>
+ * Created at:    Wed Oct 23 15:03:05 2013
+ * Modified at:   Wed Oct 23 15:19:42 2013
+ * Modified by:   Zhicong Chen <zhicong.chen@changecong.com>
+ * Status:        Experimental, do not distribute.
+ * Description:   
+ *
+ */
+
 #include <stdio.h>
 #include <string>
 #include <opencv2/opencv.hpp>
