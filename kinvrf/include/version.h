4e7fde99f817a42b1e588807f27c9b4e61a79017
Zhicong Chen <changecong@gmail.com>
2013-11-06 23:42:21 -0500

    update subversion to 3

--- kinvrf/include/version.h
+++ kinvrf/include/version.h
@@ -1,7 +1,7 @@
 /*
  * Filename:      version.h.template
  * Copyright:     2013, Zhicong Chen
- * Version:       0.2.32_8244c7f
+ * Version:       0.3.33_7fb561b
  * Author:        Zhicong Chen <zhicong.chen@changecong.com>
  * Created at:    Tue Oct 22 21:36:23 2013
  * Modified at:   Tue Oct 22 22:15:07 2013
@@ -16,8 +16,8 @@
 #define KINVRF_VERSION_H
 
 #define MAJOR_VERSION_NUMBER 0
-#define MINOR_VERSION_NUMBER 2
-#define REVERSION_NUMBER 32
-#define GIT_VERSION_NUMBER 8244c7f
+#define MINOR_VERSION_NUMBER 3
+#define REVERSION_NUMBER 33
+#define GIT_VERSION_NUMBER 7fb561b
 
 #endif KINVRF_VERSION_H
